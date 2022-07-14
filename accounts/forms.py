8efee0584ae98a19de202c261bd9df6778644557
masaya09 <74097011+masaya09@users.noreply.github.com>
2022-07-14 16:19:03 +0900

    add help_texts

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
 
@@ -8,3 +9,4 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email")
+        help_texts = {"email": _("この項目は必須です。 例：xxx@mail.com")}
